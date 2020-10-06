504bb53c6f01caa5f6bd9b5e06c7378890834b4d
Purnanand <46159709+purnanandv@users.noreply.github.com>
2020-10-06 20:14:36 +0530

    Create create_tables.sql

---
+++ CRUD/create_tables.sql
@@ -0,0 +1,10 @@
+/*Author: Purnanand Wasave*/
+
+CREATE TABLE EMPLOYEES(
+   ID   INT              NOT NULL,
+   NAME VARCHAR (30)     NOT NULL,
+   AGE  INT              NOT NULL,
+   ADDRESS  CHAR (40) ,
+   SALARY   DECIMAL (20, 4),       
+   PRIMARY KEY (ID)
+);
